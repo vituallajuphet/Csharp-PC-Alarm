e100d07dca7be43bba582ad381e72557df0fd8bd
Juphet Vitualla <vituallajuphet1114@gmail.com>
2021-05-27 22:04:16 +0800

    initial

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Mail;
+using System.Net;
+
+
+namespace securityalarm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+{
+    Port = 587,
+    Credentials = new NetworkCredential("email", "password"),
+    EnableSsl = true,
+};
+    
+smtpClient.Send("email", "recipient", "subject", "body");
+        }
+    }
+}
